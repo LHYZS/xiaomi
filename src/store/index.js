f1147b0d72dbb319631af7f815818cca1e93106f
LHYZS <1047108189@qq.com>
2018-11-19 16:53:29 +0800

    主要页面和功能已经实现

---
+++ src/store/index.js
@@ -0,0 +1,37 @@
+import Vue from 'vue'
+import Vuex from 'Vuex'
+
+Vue.use(Vuex)
+
+const state = {
+  car : [],
+  content : [],
+  productNum: 20180103,   //订单号
+};
+
+const mutations = {
+  addCar(state, obj) {
+    state.car.push(obj);
+  },
+  getProductNum(state, num) {
+    state.productNum = state.productNum + num;
+  },
+  changeNumber(state, obj) {
+    state.car.forEach(function(el) {
+      if(el.productNum === obj.productNum){
+        el.number = obj.number;
+      }
+    })
+  },
+  deleteProduct(state, num) {
+    state.car.forEach(function(el, index) {
+      if(el.productNum === num){
+        state.car.splice(index, 1);
+      }
+    })
+  }
+};
+
+export default new Vuex.Store({
+  state,mutations
+});
